fe95ae4fc06a1f9ca546f2f703f96e97b9d07903
Alameda Aztechs <frc4186@gmail.com>
2024-03-31 18:06:46 -0700

    Updated look up table

--- robot/src/main/kotlin/frc/actions/FindLaunchAngleAndSpeed.kt
+++ robot/src/main/kotlin/frc/actions/FindLaunchAngleAndSpeed.kt
@@ -3,8 +3,8 @@ package frc.actions
 import frc.vision.LimelightRunner
 
 data class AngleAndSpeed(
-    val angle: Double,
-    val speed: Double,
+        val angle: Double,
+        val speed: Double,
 )
 
 private val DefaultAngleAndSpeed = AngleAndSpeed(17.9, 0.0)
@@ -13,28 +13,34 @@ const val MAX_SPEED = 5000
 const val TEST_SPEED = MAX_SPEED * 0.20
 
 private val lookupArray = arrayOf(
-    AngleAndSpeed(17.0, 0.70 * MAX_SPEED),
-    AngleAndSpeed(17.0, 0.70 * MAX_SPEED),
-    AngleAndSpeed(21.0, 0.70 * MAX_SPEED),
-    AngleAndSpeed(26.0, 0.70 * MAX_SPEED),
-    AngleAndSpeed(28.0, 0.70 * MAX_SPEED),
-    AngleAndSpeed(30.0, 0.70 * MAX_SPEED),
-    AngleAndSpeed(32.0, 0.70 * MAX_SPEED),
-    AngleAndSpeed(37.0, 0.75 * MAX_SPEED),
-    AngleAndSpeed(34.0, 0.75 * MAX_SPEED),
-    AngleAndSpeed(37.0, 0.80 * MAX_SPEED),
-    AngleAndSpeed(38.5, 0.80 * MAX_SPEED),
-    AngleAndSpeed(38.7, 0.85 * MAX_SPEED)
+        AngleAndSpeed(17.0, 0.70 * MAX_SPEED),
+        AngleAndSpeed(19.0, 0.70 * MAX_SPEED),
+        AngleAndSpeed(23.0, 0.70 * MAX_SPEED),
+        AngleAndSpeed(28.0, 0.70 * MAX_SPEED),
+        AngleAndSpeed(32.0, 0.70 * MAX_SPEED),
+        AngleAndSpeed(34.0, 0.70 * MAX_SPEED),
+        AngleAndSpeed(34.0, 0.75 * MAX_SPEED),
+        AngleAndSpeed(39.0, 0.80 * MAX_SPEED),
+        AngleAndSpeed(38.0, 0.80 * MAX_SPEED),
+        AngleAndSpeed(38.0, 0.80 * MAX_SPEED),
+        AngleAndSpeed(38.5, 0.80 * MAX_SPEED),
+        AngleAndSpeed(38.7, 0.85 * MAX_SPEED)
 )
 
 fun findLaunchAngleAndSpeed(
-    limelight: LimelightRunner,
+        limelight: LimelightRunner,
 ): AngleAndSpeed {
     return when {
         limelight.hasTargetTag -> {
             val distanceToTag = limelight.distance
-            val roundedDistance = limelight.lookupTableRound(distanceToTag)
-            lookupArray[roundedDistance]
+            when {
+                !distanceToTag.isNaN() -> {
+                    val roundedDistance = limelight.lookupTableRound(distanceToTag)
+                    lookupArray[roundedDistance]
+                }
+
+                else -> AngleAndSpeed(0.0, 0.0)
+            }
         }
 
         else -> DefaultAngleAndSpeed
